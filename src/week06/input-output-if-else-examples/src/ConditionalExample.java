3e7f85c04d784a2991756cbf321d16698be9578a
osman-butt <butt.osman@gmail.com>
2025-02-04 14:59:13 +0100

    Pseudocode for live session

---
+++ src/week06/input-output-if-else-examples/src/ConditionalExample.java
@@ -0,0 +1,32 @@
+public class ConditionalExample {
+    public static void main(String[] args) {
+        // Correct Usernames and passwords
+        final String CORRECT_ADMIN_USERNAME = "admin";
+        final String CORRECT_ADMIN_PASSWORD = "1234";
+        final String CORRECT_EMPLOYEE_USERNAME = "empolyee";
+        final String CORRECT_EMPLOYEE_PASSWORD = "1234";
+
+        // Input fx from keyboard via Scanner
+        String username = "empolyee";
+        String password = "1234";
+
+        // Check if admin (boolean)
+
+        // Check if employee (boolean)
+
+        // Simple if (check if admin)
+
+        // Simple if-else (check if admin else wrong user)
+
+        // Chained if-else (check if admin or employee else wrong user)
+
+        // Nested if-else (Check if logged in (if admin else) else wrong user)
+    }
+}
+// TODO:
+// - Add if
+// - extract logic to booleans
+// - Add if-else
+// - Add chained with else if
+// - Add nested
+// - If time add scanner for input
---
+++ src/week06/input-output-if-else-examples/src/ScannerExample.java
@@ -0,0 +1,21 @@
+public class ScannerExample {
+    public static void main(String[] args) {
+        // Opret et scanner objekt
+
+        // Prompt brugeren for input af navn
+
+        // Promt brugeren for input af alder
+
+        // Tjek for alder >= 18
+
+        // Print det hele
+
+        // Er du okay (boolean)?
+    }
+}
+
+// Udvidelser
+// - int inputAge = scanner.nextLine();
+// - Prøv String inputAge = scanner.nextLine();
+// - Ulempe: Vi kan ikke lave operationer på inputAge nu fx (inputAge>18)
+// - Luk scanner og forsøg at få et input - Runtime error
