1d52c5042312d69bbd13270b1544576eab4e0854
osman-butt <butt.osman@gmail.com>
2025-02-10 11:27:36 +0100

    solutions

--- src/week07/src/exercises/methods/Calculator.java
+++ src/week07/src/exercises/methods/Calculator.java
@@ -2,6 +2,30 @@ package exercises.methods;
 
 public class Calculator {
     public static void main(String[] args) {
+        double addTest = add(12,4);
+        double subtractionTest = subtraction(12,4);
+        double multiplicationTest = multiply(12,4);
+        double divisionTest = division(12,4);
 
+        System.out.println(addTest);
+        System.out.println(subtractionTest);
+        System.out.println(multiplicationTest);
+        System.out.println(divisionTest);
+    }
+
+    public static double add(double a, double b) {
+        return a+b;
+    }
+
+    public static double subtraction(double a, double b) {
+        return a-b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double numerator, double denominator) {
+        return numerator / denominator;
     }
 }
--- src/week07/src/exercises/methods/FutureAge.java
+++ src/week07/src/exercises/methods/FutureAge.java
@@ -6,32 +6,35 @@ public class FutureAge {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-//        String name = getName(scanner);
-//        int birthYear = getBirthYear(scanner);
-
-//        int ageInFuture = calculateAgeInFuture(birthYear, 2050);
-//        printMessage(name, ageInFuture);
-
+        String name = getName(scanner);
+        int birthYear = getBirthYear(scanner);
+        int ageInFuture = calculateAgeInFuture(birthYear, 2050);
+        printMessage(name, ageInFuture);
         scanner.close();
     }
-
     // TODO:
     // Lav en metode getName der:
     // - Modtager et scanner objekt som parameter
     // - Spørg brugeren om et navn
-    // - Returner navnet som en String
+    // - Returner navnet som String
+    public static String getName(Scanner mitScannerObjekt) {
+        System.out.println("Hvad er dit navn?");
+        return mitScannerObjekt.nextLine();
+    }
 
     // TODO:
-    // Lav én metode getBirthYear der:
+    // Lav en metode getBirthYear der:
     // - Modtager et scanner objekt som parameter
     // - Spørg brugeren om deres fødselsår
-    // - Returner fødselsåret som et int
+    // - Returner fødselsåret som int
+    public static int getBirthYear(Scanner sc) {
+        System.out.println("Hvornår er du født?");
+        return sc.nextInt();
+    }
 
     public static int calculateAgeInFuture(int birthYear, int futureYear) {
         return futureYear - birthYear;
     }
-
     public static void printMessage(String name, int ageInFuture) {
         System.out.println(name + ", du vil være " + ageInFuture + " år gammel i 2050.");
     }
