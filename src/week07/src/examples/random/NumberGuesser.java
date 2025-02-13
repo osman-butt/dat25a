e1716f86727ff9ec878b40c6000dd87de82b6233
osman-butt <butt.osman@gmail.com>
2025-02-13 11:22:29 +0100

    Live code start - numberguesser

---
+++ src/week07/src/examples/random/NumberGuesser.java
@@ -0,0 +1,24 @@
+package examples.random;
+
+public class NumberGuesser {
+    public static void main(String[] args) {
+
+        // Create a Random object to generate random numbers
+
+        // Create a Scanner object to read input from the user
+
+        // Declare variables
+        // - randomNumber: The random number to be guessed
+        // - attemptsLeft: The number of attempts allowed
+
+        // Generate a random number between 1 and 100
+
+        // Game loop: keep guessing until correct or attempts run out
+
+        // Prompt the user to enter a guess
+
+        // Check if the guess is correct
+
+        // Display the attemptsLeft as score at the end of the game
+    }
+}
